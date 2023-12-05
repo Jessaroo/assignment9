88da149518c900f2f72c664418c3f924c417c57c
Jessaroo <j_caetana@yahoo.com>
2023-12-05 15:29:58 -0800

    Answered questions.

---
+++ script.js
@@ -0,0 +1,27 @@
+// 1. Write a console.log() statement that answers the following question: What worked
+// well for you over the last 9 weeks that aided in your learning process?
+
+console.log(
+  "What worked well for you over the last 9 weeks that aided in your learning process?"
+);
+console.log("I think that having already learned JavaScript earlier this year helped me a lot. I was able to learn more about JavaScript so it's finally making more sense to me.");
+
+// 2. Write a console.log() statement that answers the following question: What would you
+// change about the course?
+
+console.log("What would you change about the course?");
+console.log("I don't know what I would change about the course. JavaScript is just really hard to learn from what I have heard. I have been struggling with it and a lot of it didn't make sense to me so this is my second class to learn JavaScript and it's just barely starting to make sense. I still don't fully understand it but I do like how we went in depth on each topic and had reading. There was a lot more reading on some weeks but maybe that is what is helping me to understand it better.");
+
+// 3. Write a console.log() statement that answers the following question: Did you learn
+// what you expected to learn in this course?
+
+console.log("Did you learn what you expected to learn in this course?");
+console.log("I wanted to actually understand what I was doing when I was writing functions because none of it made sense to me prior to taking this class. It definitely is making more sense to me now but I still need to learn a lot more. ");
+
+// 4. Write a console.log() statement that answers the following question: What concepts
+// were new to you and what concepts had you encountered already in the past?
+
+console.log(
+  "What concepts were new to you and what concepts had you encountered already in the past?"
+);
+console.log("None of the concepts were new to me, however I did learn more about everything and I'm understanding more now. I have honestly struggled with everything in JavaScript so I feel that it's getting easier for me to write functions now.");
